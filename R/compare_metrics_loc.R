7d6937e32f5ef55b50662d8ee8562b27114a5814
Francesca Buffa <francesca.buffa@oncology.ox.ac.uk>
2017-10-15 13:08:05 +0000

    version 0.1.11

---
+++ R/compare_metrics_loc.R
@@ -0,0 +1,191 @@
+# compare_metrics_loc.R
+#
+# This function creates the plots to compare various signature summary statistic metrics against each other
+# That is, compares the mean, median and first principal component with each other, and also produces PCA vs variance
+# plot for the first 10 principal components of the dataset
+# @param gene_sigs_list A list of genes representing the gene signature to be tested.
+# @param names_sigs The names of the gene signatures (one name per gene signature, in gene_sigs_list)
+# @param mRNA_expr_matrix A list of expression matrices
+# @param names_datasets The names of the different datasets contained in mRNA_expr_matrix
+# @param out_dir A path to the directory where the resulting output files are written
+# @param file File representing the log file where errors can be written
+# @param showResults Tells if open dialog boxes showing the computed results. Default is FALSE
+# @param radar_plot_values A list of values that store computations that will be used in the final summary radarplot
+# @keywords compare_metrics_loc
+
+compare_metrics_loc <- function(gene_sigs_list,names_sigs, mRNA_expr_matrix, names_datasets, out_dir = '~',file=NULL,showResults = FALSE,radar_plot_values){
+  # require(gplots)
+  dir.create(file.path(out_dir,'metrics_tables'))
+  for(k in 1:length(names_sigs)){ 
+    #for each signature we will make a separate file comparing the datasets
+    gene_sig <- gene_sigs_list[[names_sigs[k]]] # load the gene signature
+    if(is.matrix(gene_sig)){gene_sig = as.vector(gene_sig);}
+    #set up canvas for plotting
+
+    if (showResults){
+      grDevices::dev.new()
+    }else{
+      grDevices::pdf(file.path(out_dir,paste0('sig_compare_metrics_',names_sigs[k],'.pdf')),width=3*length(names_datasets),height=10)
+    }
+
+    #find the max number of characters in the title
+    max_title_length <- -999
+    for( i in 1:length(names_datasets)){
+      if(max_title_length < nchar(paste0(names_datasets[i],' ',names_sigs[k]))){
+        max_title_length <- nchar(paste0(names_datasets[i],' ',names_sigs[k]))
+      }
+    }
+    #set up the canvas
+    graphics::par(mfcol = c(4,length(names_datasets)),mar=c(4,4,4,4))
+
+    for ( i in 1:length(names_datasets)){
+      # now we can loop over the datasets for the plot and generate the metrics for every dataset with this signature
+      data.matrix = mRNA_expr_matrix[[names_datasets[i]]] #load the data
+      data.matrix[!(is.finite(as.matrix(data.matrix)))] <- NA #ensure that the data is not infintie
+      inter = intersect(gene_sig,rownames(data.matrix)) #consider only the genes actually present in the data
+
+      med_scores <- apply(data.matrix[inter,],2,function(x){stats::median(stats::na.omit(x))}) #compute median
+      mean_scores <- apply(data.matrix[inter,],2,function(x){mean(stats::na.omit(x))}) #compute mean
+      
+      pca1_scores <- NULL
+
+      tryCatch({
+        pca1_scores <- stats::prcomp(stats::na.omit(t(data.matrix[inter,])),retx=T) #compute PCA1
+        
+        },error=function(e){
+          pca1_scores <<- NULL
+          cat(paste0("There was an error:  ",names_datasets[i]," ", names_sigs[k]," ", e,'\n'), file=file)
+
+     #     print(paste0("error: ", e))
+      })
+
+
+      # print(paste0("test ",pca1_scores ))
+      if(length(pca1_scores) > 1){#!is.null(pca1_scores)){
+        props_of_variances <- pca1_scores$sdev^2/(sum(pca1_scores$sdev^2)) #for the scree plot
+        pca1_scores <- pca1_scores$x[,1] #takes the actual PCA1 scores
+        common_score_cols <- intersect(names(med_scores),intersect(names(mean_scores),names(pca1_scores))) #ensures we have the same samples for each plot
+      }else{
+        common_score_cols <- intersect(names(med_scores),names(mean_scores))
+      }
+
+      if(length(common_score_cols) > 1){
+        #the following is the colourmap for the 2D scatter
+        jet.colors <- grDevices::colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
+        #plotting commands for the 2D scatterplot for mean-median correlation as follows
+        graphics::smoothScatter(med_scores[common_score_cols],mean_scores[common_score_cols],colramp = jet.colors,xlab=NA,ylab=NA,main='Median vs Mean')
+        graphics::points(med_scores[common_score_cols],mean_scores[common_score_cols],pch='.') #draw the points on top of it
+        graphics::par(new=T)#,srt=45)
+        graphics::plot(stats::density(med_scores[common_score_cols]), axes=F, xlab=NA, ylab=NA,  col='red',main=NA,lwd=2) #draw the density plot behind it
+        graphics::axis(side = 4)
+        graphics::mtext(side = 4, line = 2, 'Density',cex=0.8) #labels for the axes
+        graphics::mtext(side = 2, line = 2, 'Mean',cex=0.8)
+        graphics::mtext(side = 1, line = 2, 'Median',cex=0.8)
+        graphics::mtext(side=3,line=2.5,paste0(names_datasets[i],' ',names_sigs[k]),cex=min(1,3*10/max_title_length)) #title
+        rho <- stats::cor(med_scores[common_score_cols],mean_scores[common_score_cols],method='spearman') 
+        rho_mean_med <- rho
+        graphics::mtext(paste0('rho = ',format(rho,digits = 2)),side=3,line=0,cex = 0.6,at=max(med_scores[common_score_cols]))
+      }else{
+        graphics::plot.new()
+        graphics::mtext(side=3,line=2.5,paste0(names_datasets[i],' ',names_sigs[k]),cex=min(1,3*10/max_title_length)) #title
+
+        graphics::title(paste0('\n\nToo many NA values for Mean/Median in \n',names_datasets[i],' ',names_sigs[k]))#cex=min(1,4*10/max_title_length))
+        rho_mean_med <- 0
+      }
+      if (length(pca1_scores) > 1){#(!is.null(pca1_scores)){
+        #plotting for the mean-pca1
+        graphics::smoothScatter(mean_scores[common_score_cols],pca1_scores[common_score_cols],colramp = jet.colors,xlab=NA,ylab=NA,main='Mean vs PCA1')
+        graphics::points(mean_scores[common_score_cols],pca1_scores[common_score_cols],pch='.')
+        graphics::par(new=T)
+        graphics::plot(stats::density(mean_scores[common_score_cols]), axes=F, xlab=NA, ylab=NA, col='red',main=NA,lwd=2)
+        graphics::axis(side = 4)
+        graphics::mtext(side = 4, line = 2, 'Density',cex=0.8)
+        rho <- stats::cor(mean_scores[common_score_cols],pca1_scores[common_score_cols],method='spearman')
+        rho_mean_pca1 <- rho
+        graphics::mtext(paste0('rho = ',format(rho,digits = 2)),side=3,line=0,cex = 0.6,at=max(mean_scores[common_score_cols]))
+        graphics::mtext(side = 2, line = 2, 'PCA1',cex=0.8)
+        graphics::mtext(side = 1, line = 2, 'Mean',cex=0.8)
+
+        #plotting for the pca1-median
+        graphics::smoothScatter(pca1_scores[common_score_cols],med_scores[common_score_cols],colramp = jet.colors,xlab=NA,ylab=NA,main='PCA1 vs Median')
+        graphics::points(pca1_scores[common_score_cols],med_scores[common_score_cols],pch='.')
+        graphics::par(new=T)
+        graphics::plot(stats::density(pca1_scores[common_score_cols]), axes=F, xlab=NA, ylab=NA, col='red',main=NA,lwd=2)
+        graphics::axis(side = 4)
+        graphics::mtext(side = 4, line = 2, 'Density',cex=0.8)
+        rho <- stats::cor(pca1_scores[common_score_cols],med_scores[common_score_cols],method='spearman')
+        rho_pca1_med <- rho
+        graphics::mtext(paste0('rho = ',format(rho,digits = 2)),side=3,line=0,cex = 0.6,at=max(pca1_scores[common_score_cols]))
+        graphics::mtext(side = 2, line = 2, 'Median',cex=0.8)
+        graphics::mtext(side = 1, line = 2, 'PCA1',cex=0.8)
+      }else{
+
+#        graphics::par(new=T)
+        graphics::plot.new()
+        graphics::title(paste0('\n\nToo many NA values for PCA1/Mean in \n',names_datasets[i],' ',names_sigs[k]))#cex=min(1,4*10/max_title_length))
+        rho_mean_pca1 <- 0
+
+ #       graphics::par(new=T)
+        graphics::plot.new()
+        graphics::title(paste0('\n\nToo many NA values for Median/PCA1 in \n',names_datasets[i],' ',names_sigs[k]))#cex=min(1,4*10/max_title_length))
+        rho_pca1_med <- 0
+
+      }
+      #here we put the cor plot of the mean median and pca1 (if we wanted an autocorrelation heatmap of these metrics)
+
+      # autocors_mat <- matrix(0,nrow=3,ncol=3)
+      # row.names(autocors_mat) <- c('Mean' , 'Median','PCA1')
+      # colnames(autocors_mat) <- c('Mean' , 'Median','PCA1')
+      # autocors_mat[1,1] <- 1
+      # autocors_mat[1,2] <- rho_mean_med
+      # autocors_mat[1,3] <- rho_mean_pca1
+
+      # autocors_mat[2,1] <- rho_mean_med
+      # autocors_mat[2,2] <- 1
+      # autocors_mat[2,3] <- rho_pca1_med
+
+      # autocors_mat[3,1] <- rho_mean_pca1
+      # autocors_mat[3,2] <- rho_pca1_med
+      # autocors_mat[3,3] <- 1
+
+      #here we output the table of mean, median and pca1 scores for each sample to a table
+      if(length(pca1_scores) > 1){#(!is.null(pca1_scores)){
+        output_mat <- cbind(mean_scores[common_score_cols],cbind(med_scores[common_score_cols],pca1_scores[common_score_cols]))
+        colnames(output_mat) <- c('Mean_Scores','Median_Scores','PCA1_Scores')
+      }else{
+        output_mat <- cbind(mean_scores[common_score_cols],med_scores[common_score_cols])
+        colnames(output_mat) <- c('Mean_Scores','Median_Scores')
+
+      }
+
+      utils::write.table(output_mat,file=file.path(out_dir,'metrics_tables', paste0('metrics_table_',names_sigs[k],'_',names_datasets[i],'.txt')),quote=F,sep='\t')
+
+      #stores values that will be used in the radarplot
+      radar_plot_values[[names_sigs[k]]][[names_datasets[i]]]['rho_mean_med'] <- rho_mean_med
+      radar_plot_values[[names_sigs[k]]][[names_datasets[i]]]['rho_pca1_med'] <- rho_pca1_med
+      radar_plot_values[[names_sigs[k]]][[names_datasets[i]]]['rho_mean_pca1'] <- rho_mean_pca1
+      
+      if(length(pca1_scores) > 1){#(!is.null(pca1_scores)){
+        #draws the scree plot
+        bars_plot <- props_of_variances[1:min(10,length(props_of_variances))]
+        graphics::barplot(bars_plot,main="PCA vs proportion\n of variance") #ylim= c(0,1),
+        graphics::mtext(side = 1, line = 2, 'PCA',cex=0.8)
+        radar_plot_values[[names_sigs[k]]][[names_datasets[i]]]['prop_pca1_var']<- bars_plot[1]
+      }else{
+       # graphics::par(new=T)
+        graphics::plot.new()
+        graphics::title(paste0('\n\nToo many NA values for PCA1 in \n',names_datasets[i],' ',names_sigs[k]))#cex=min(1,4*10/max_title_length))
+        radar_plot_values[[names_sigs[k]]][[names_datasets[i]]]['prop_pca1_var']<- 0
+      }
+    }
+    #saves file
+    if(showResults){
+      grDevices::dev.copy(grDevices::pdf,file.path(out_dir,paste0('sig_compare_metrics_',names_sigs[k],'.pdf')),width=3*length(names_datasets),height=10)
+    }
+    if(grDevices::dev.cur()!=1){
+        g <- grDevices::dev.off() # to reset the graphics pars to defaults
+    }
+    }
+  cat('Metrics compared successfully.\n', file=file) #output to log
+  radar_plot_values #returns the radarplot values
+}
---
+++ R/make_all_plots.R
@@ -0,0 +1,258 @@
+#' make_all_plots.R
+#'
+#' Makes all the plots for the quality control of the list(s) of genes.
+#'
+#' @param gene_sigs_list A list of genes representing the gene signature to be tested.
+#' @param mRNA_expr_matrix A list of expression matrices
+#' @param names_sigs The names of the gene signatures (e.g. Hypoxia, Invasiveness), one name per each signature in gene_sigs_list.
+#' @param names_datasets The names of the different datasets contained in mRNA_expr_matrix
+#' @param covariates A list containing a sub-list of 'annotations' and 'colors' which contains the annotation matrix for the given dataset and the associated colours with which to plot in the expression heatmap
+#' @param thresholds A list of thresholds to be considered for each data set, default is median of the data set. A gene is considered expressed if above the threshold, non-expressed otherwise. One threshold per dataset, in the same order as the dataset list.
+#' @param out_dir A path to the directory where the resulting output files are written
+#' @param showResults Tells if open dialog boxes showing the computed results. Default is FALSE
+#' @param origin Tells if datasets have come from different labs/experiments/machines. Is a vector of characters, with same character representing same origin. Default is assumption that all datasets come from the same source.
+#' @param doNegativeControl Logical, tells the function if negative controls must be computed. TRUE by default.
+#' @param numResampling Number of re-samplings (50 by default) during negative controls
+#' @keywords make_all_plots
+#' @export
+#' @examples
+#' library(sigQC)
+#' names = c("dataset1")
+#' data.matrix = replicate(10, rnorm(20))#random matrix - 10 genes x 20 samples
+#' mRNA_expr_matrix = list()
+#' mRNA_expr_matrix[["dataset1"]] = data.matrix
+#' row.names(mRNA_expr_matrix$dataset1) <- as.character(1:(dim(mRNA_expr_matrix$dataset1)[1]))
+#' colnames(mRNA_expr_matrix$dataset1) <- as.character(1:(dim(mRNA_expr_matrix$dataset1)[2]))
+#' #Define the signature
+#' gene_sigs_list = list()
+#' signature = "hypoxiaSig"
+#' gene_sig = c('1', '4', '5')#gene ids
+#' gene_sigs_list[[signature]] = as.matrix(gene_sig)
+#' names_sigs = c(signature)
+#' make_all_plots(gene_sigs_list = gene_sigs_list, mRNA_expr_matrix = mRNA_expr_matrix, 
+#'     doNegativeControl=FALSE, out_dir = tempdir(), showResults=FALSE)
+
+
+
+make_all_plots <- function(gene_sigs_list, mRNA_expr_matrix,names_sigs=NULL,names_datasets=NULL , covariates=NULL, thresholds=NULL, out_dir = tempdir(), showResults = TRUE,origin=NULL, doNegativeControl=TRUE, numResampling=50){
+
+  #### print version number
+  #### encoding scheme: major.minor
+  #### major for large change
+  #### minor for small change, whose results are expected to be similar as previous version. (two digits)
+  print("-----sigQC Version 0.1.17-----")
+
+  ###########Check the input
+ radar_plot_values <- list();
+  #check that there is a list of gene signatures
+  if(missing(gene_sigs_list)){
+    stop("Need to specify a list of gene signatures. The IDs must match those in the expression matrices.")
+  }
+  #check that there is a/are dataset(s)
+  if(missing(mRNA_expr_matrix)){
+    stop("Need to specify a list of expression matrices.
+         Please note that each element of the list must have a
+         name matching a value in the 'names' input parameter.")
+  }
+
+  #check that datasets have names, otherwise give them same names as in names_datasets
+  if(is.null(names_datasets)){
+    if(is.null(names(mRNA_expr_matrix))){
+      stop("Need to specify a list of names. Each value must match a
+           name in the mRNA_expr_matrix list of expression matrices.")
+    } else {
+      names_datasets <- names(mRNA_expr_matrix)
+    }
+  } else {
+    if(is.null(names(mRNA_expr_matrix))){
+       names(mRNA_expr_matrix) <- names_datasets;
+     }
+   }
+  #check that signatures have names, otherwise give them same names as in names_sigs
+  if(is.null(names_sigs)){
+    if(is.null(names(gene_sigs_list))){
+      stop("Need to specify a list of names. Each value must match a
+           name in the gene_sigs_list list of gene signatures.")
+    } else {
+      names_sigs <- names(gene_sigs_list)
+    }
+  }
+  #set up the variable that will hold values for the final radarplot
+  for(i in 1:length(names_sigs)){
+    radar_plot_values[[names_sigs[i]]] <- list();
+   }
+
+   #need to check that the length of the origins variable is the same as the number of datasets
+   if (!is.null(origin)){
+    if (length(origin)!=length(names_datasets)){
+      stop("Need to specify an origin for each dataset present, in the order the datasets are listed.")
+    }
+    if(sum(as.numeric(table(origin)) < 2) > 0){
+      stop("Need to ensure that there are at least 2 datasets from each origin.")
+    }
+  }
+  tryCatch({
+    #Check if the OS is windows and allow longer path in this case
+    os.name = Sys.info()["sysname"]
+    os.release = Sys.info()["release"]
+    useVirDrive = F
+    if(doNegativeControl && nchar(out_dir)>100 && os.name=="Windows" && grepl(paste0(".*(7|8|10).*"), os.release, ignore.case=TRUE)){
+       #Check if there is a letter not already in use to create a virtual drive
+      freeLetter = FALSE;
+      for(letter in letters){
+         path.test=paste0(letter,":/");
+         if(!dir.exists(path.test)){
+           freeLetter = TRUE;
+           break;
+         }
+       }
+       if(freeLetter==TRUE){
+         virD = paste0(letter, ":")
+         system(paste0("subst ",virD, " ", out_dir), intern=T)
+         out_dir = virD
+         # outDir.file = file.path(outDir, paste0(paste(sample(letters, 251, TRUE), collapse = ''), '.txt'))
+         # write(1, file = outDir.file)
+         useVirDrive = T;
+       }else{
+         stop("ERROR: There is a problem with the output path length. Try to change the output directory (look for a short path) or use a different OS.");
+       }
+     # #out_dir = path.expand("~")
+      # # out_dir1 = file.path("/?", out_dir)
+      # #
+      # # out_dir1 = paste("\\?\\", out_dir, sep="")
+      # # out_dir2 = file.path("\\\\?\\C:\\Users\\Alessandro\\Documents")
+      # virD = "y:"
+      # system(paste0("subst ",virD, " ", out_dir), intern=T)
+      # out_dir = virD
+      # # outDir.file = file.path(outDir, paste0(paste(sample(letters, 251, TRUE), collapse = ''), '.txt'))
+      # # write(1, file = outDir.file)
+      # useVirDrive = T;
+    }
+
+       #check that the legnths of the names are all equal
+    if ((length(names_datasets)== length(mRNA_expr_matrix)) && (length(names_sigs)==length(gene_sigs_list))){
+
+
+      ###########Check if the needed package exists otherwise install it
+      dir.create(out_dir)
+     # utils::write.table('',file=file.path(out_dir, "log.log"))
+      #LOG file path
+      logfile.path = file.path(out_dir, "log.log")
+      #Log conn
+      log.con = file(logfile.path, open = "a") #open the logfile
+      #run each of the sub functions
+      cat(paste("LOG FILE CREATED: ",Sys.time(), sep=""), file=log.con, sep="\n") #start the log file
+
+      #check that the signatures are of length >= 2, if not, we remove it
+      sigs_to_remove_ind <- c()
+      for (k in 1:length(names_sigs)){
+        gene_sig <- gene_sigs_list[[names_sigs[k]]] #load in the gene signature
+        if(is.matrix(gene_sig)){gene_sig=as.vector(gene_sig);}
+        if(length(gene_sig) < 2){
+          sigs_to_remove_ind <- c(sigs_to_remove_ind,k)
+  #        stop("Every signature must contain at least 2 elements.")
+         }
+      }
+      #remove the offending signatures
+      if (length(sigs_to_remove_ind) > 0){
+        gene_sigs_list[names_sigs[k]] <- NULL
+        for(j in 1:length(sigs_to_remove_ind)){
+          cat(paste0("Need at least 2 elements per signature (signature removed): ",names_sigs[sigs_to_remove_ind[j]]), file=log.con, sep="\n")
+        }
+        names_sigs <- names_sigs[-sigs_to_remove_ind]#NULL
+      }
+
+      if(length(names_sigs) == 0){
+        stop("No signatures contained at least 2 signature elements.") #if all signatures have been removed, then we have to stop
+      }
+
+      #first we must check that the genes of the signature are actually present in the datasets...
+      datasets_to_remove_ind <- c()
+      for (k in 1:length(names_sigs)){
+        gene_sig <- gene_sigs_list[[names_sigs[k]]] #load in the gene signature
+        if(is.matrix(gene_sig)){gene_sig=as.vector(gene_sig);}
+        for(i in 1:length(names_datasets)){
+          data.matrix = mRNA_expr_matrix[[names_datasets[i]]] #load in the data matrix
+          inter <- intersect(gene_sig, row.names(data.matrix)) #make sure the genes are present in the dataset
+          if(length(inter) < 2){
+            #we need to exclude this dataset from further analysis because not enough signature genes are present
+            datasets_to_remove_ind <- c(datasets_to_remove_ind,i)
+          }
+        }
+      }
+      datasets_to_remove_ind <- unique(datasets_to_remove_ind)
+      if(length(datasets_to_remove_ind) > 0) {
+        #now we do the removal
+        mRNA_expr_matrix[names_datasets[datasets_to_remove_ind]] <- NULL
+        #write it out that we've removed these datasets
+        for(j in 1:length(datasets_to_remove_ind)){
+          cat(paste0("Not enough signature genes present in dataset (dataset removed): ",names_datasets[datasets_to_remove_ind[j]]), file=log.con, sep="\n")
+        }
+        names_datasets <- names_datasets[-datasets_to_remove_ind]#NULL
+      }
+
+      if(length(names_datasets) == 0){
+        stop("No datasets expressed at least 2 signature elements.") #if all datasets have been removed, then we have to stop
+      }
+
+      tryCatch(radar_plot_values <- eval_var_loc(gene_sigs_list,names_sigs, mRNA_expr_matrix,names_datasets,out_dir,file=log.con,showResults,radar_plot_values),
+               error=function(err){
+                 cat(paste0("Error occurred in eval_var_loc: ",err), file=log.con, sep="\n")
+               })
+      tryCatch(radar_plot_values <- eval_expr_loc(gene_sigs_list,names_sigs,mRNA_expr_matrix,names_datasets,thresholds, out_dir,file=log.con,showResults,radar_plot_values ),
+               error=function(err){
+                 cat(paste0("Error occurred in eval_expr_loc: ",err), file=log.con, sep="\n")
+               })
+      tryCatch(radar_plot_values <- eval_compactness_loc(gene_sigs_list,names_sigs,mRNA_expr_matrix,names_datasets,out_dir,file=log.con,showResults,radar_plot_values,logged=T,origin ),
+               error=function(err){
+                 cat(paste0("Error occurred during the evaluation of compactness: ",err), file=log.con, sep="\n")
+               })
+      tryCatch({radar_plot_values <- compare_metrics_loc(gene_sigs_list,names_sigs,mRNA_expr_matrix,names_datasets,out_dir,file=log.con,showResults,radar_plot_values )},
+               error=function(err){
+              #  print(paste0("Error, likely due to inability to calculate PCA, because of missing values: ", err))
+                 cat(paste0("Error occurred, likely due to inability to calculate PCA, because of missing values:  ",err), file=log.con, sep="\n")
+               })
+      tryCatch({radar_plot_values <- eval_stan_loc(gene_sigs_list,names_sigs,mRNA_expr_matrix,names_datasets,out_dir,file=log.con,showResults,radar_plot_values )},
+            error=function(err){
+             cat(paste0("Error occurred in eval_stan_loc: ",err), file=log.con, sep="\n")
+           })
+      tryCatch(radar_plot_values <- eval_struct_loc(gene_sigs_list,names_sigs,mRNA_expr_matrix,names_datasets,covariates,out_dir,file=log.con,showResults,radar_plot_values ),
+               error=function(err){
+                 cat(paste0("Error occurred in eval_struct_loc: ",err), file=log.con, sep="\n")
+               })
+      # print(radar_plot_values)
+      tryCatch(make_radar_chart_loc(radar_plot_values,showResults,names_sigs, names_datasets,out_dir,file=log.con),
+               error=function(err){
+                 cat(paste0("Error occurred in make_radar_chart_loc: ",err), file=log.con, sep="\n")
+               })
+      if(doNegativeControl){
+        tryCatch(.sigsQcNegativeControl(genesList=gene_sigs_list, expressionMatrixList=mRNA_expr_matrix, outputDir=out_dir, logFile=logfile.path, numResampling=numResampling),
+          error=function(err){
+            cat(paste0("Error occurred during the execution of negative controls: ",err), file=log.con, sep="\n")
+          }
+        )
+      }
+
+
+
+      if(!showResults){
+         g<- grDevices::dev.off(grDevices::dev.list())
+      }
+    
+      # close(log.con) #close connection to the file
+    }else{
+      print("Error: the length of names is not matching the number of elements in the expression matrices list.
+            You need to have a name for every dataset and signature used.")
+    }
+  }, error = function(err) {
+    #cat("", file=log.con, sep="\n")
+    #cat(paste(Sys.time(),"Errors occurred during in sigQcNegativeControl:", err, sep=" "), file=log.con, sep="\n")
+    #stop("Errors occurred during the computation of negative controls")
+  }, finally = {
+    #cat("---------------------------------------------------------------------------", file=log.con, sep="\n")
+    if(useVirDrive){
+      system(paste0("subst ",virD," /D"), intern =T)
+    }
+    close(log.con)
+  })#END tryCatch
+}
